2bc84e58785b1509bdcdaabadd00995770920f2d
Manish <pramanish143@gmail.com>
2026-08-31 11:22:09 +0530

    RBAC

---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Connect to database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

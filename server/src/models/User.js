3c17f0d3081e5ee50e8bd0e04d3b2854365fea52
Manish <pramanish143@gmail.com>
2026-08-31 10:06:11 +0530

    backend

---
+++ server/src/models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("User", UserSchema);
